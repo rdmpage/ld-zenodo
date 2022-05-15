3fc5d5ecc15c9b39f6bac4055bb4a9995d85f2d3
Roderic Page <rdmpage@gmail.com>
2022-05-15 15:33:50 +0100

    First commit

---
+++ fetch.php
@@ -0,0 +1,31 @@
+<?php
+
+// fetch data using a list of ids and store in cache
+
+require_once(dirname(__FILE__) . '/core.php');
+
+$filename = "ids.txt";
+
+$force = false; // true if always grab new copy
+$fresh = false; // true if refresh older copies
+
+$file_handle = fopen($filename, "r");
+while (!feof($file_handle)) 
+{
+	$id = trim(fgets($file_handle));
+	
+	$id = str_replace('oai:zenodo.org:', '', $id);
+	
+	if ($id != '')
+	{
+	
+		$data = get_one($id, $force, $fresh);
+	
+		if ($data)
+		{
+			echo $data;
+		}
+	}	
+}	
+
+?>
